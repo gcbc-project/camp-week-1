600eab488fa461f18a2225a94ff7c3dd5189f7b5
qaws1234plok <qaws915@gmail.com>
2024-04-15 16:37:54 +0900

    feature/card-logic

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -10,7 +10,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Time.timeScale = 1.0f;
     }
 
     // Update is called once per frame
