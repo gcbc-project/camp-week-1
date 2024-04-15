2dc6a25512b1b763da46256b7238cff2ea249fe6
dldlsgh97 <45443021+dldlsgh97@users.noreply.github.com>
2024-04-15 17:06:30 +0900

    Board  스크립트 업데이트
    이미지 개수 30개로 증가,
    (이미지 위치 조정 필요)

--- Assets/Script/Board.cs
+++ Assets/Script/Board.cs
@@ -8,12 +8,12 @@ public class Board : MonoBehaviour
     public GameObject Card;
     void Start()//카드 랜덤 배열전 인덱스 지정
     {
-        int[] arr = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7 };
-        arr = arr.OrderBy(x => Random.Range(0f, 7f)).ToArray();
+        int[] arr = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11, 12, 12, 13, 13, 14, 14, 15, 15  };
+        arr = arr.OrderBy(x => Random.Range(0f, 15f)).ToArray();
 
-        for (int i = 0; i < 16; i++)
+        for (int i = 0; i < 30; i++)
         {
-            GameObject go = Instantiate(Card, this.transform);
+            GameObject go = Instantiate(Card, this.transform); // 카드배열 위치 조정 필요
             float x = (i % 4) * 1.4f - 2.1f;
             float y = (i / 4) * 1.4f - 3.0f;
             go.transform.position = new Vector2(x, y);
