5b7974efa0dfc428f351ed504b5116fe5eb538d5
dldlsgh97 <45443021+dldlsgh97@users.noreply.github.com>
2024-04-15 16:56:58 +0900

    Board 스크립트 생성
    Borad스크립트(카드를 랜덤으로 배치) 생성

---
+++ Assets/Script/Board.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public GameObject Card;
+    void Start()//카드 랜덤 배열전 인덱스 지정
+    {
+        int[] arr = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7 };
+        arr = arr.OrderBy(x => Random.Range(0f, 7f)).ToArray();
+
+        for (int i = 0; i < 16; i++)
+        {
+            GameObject go = Instantiate(Card, this.transform);
+            float x = (i % 4) * 1.4f - 2.1f;
+            float y = (i / 4) * 1.4f - 3.0f;
+            go.transform.position = new Vector2(x, y);
+            go.GetComponent<Card>().OnCardSetting(arr[i]);
+        }
+        /* GameManager 싱글톤화 이후 주석 해제
+        GameManager.instance.cardCount = arr.Length;
+        */
+    }
+
+
+}
+
+
+}
