38ce1cb38a7abe9f15070f6d2f394fc9d8453eca
YooSeungA52 <124012310+YooSeungA52@users.noreply.github.com>
2024-04-16 14:53:42 +0900

    시간 경과에 따른 배경색 변경
    게임 시간 25초가 되면 배경색이 바뀌는 기능 추가

--- Assets/Script/Timer.cs
+++ Assets/Script/Timer.cs
@@ -7,15 +7,21 @@ public class Timer : MonoBehaviour
 {
     public Text TimeTxt;
     public Animator TimeTxtAnim;
-
-    public float WarningTime = 20.0f; //애니메이션 시작할 시간
+    public float WarningTime = 20.0f; //타이머 애니메이션 시작할 시간
+    public float WarningBackground = 25.0f; //애니메이션 시작할 시간
 
     private GameManager _gameManager; //게임매니저에 접근하기 위한 변수
+    private Camera _mainCamera; //메인카메라
+
+    public Color StartColor;
+    public Color EndColor;
+    public float ColorChangeDuration = 10.0f; //배경색 변경에 걸리는 시간
 
     // Start is called before the first frame update
     void Start()
     {
         _gameManager = FindObjectOfType<GameManager>(); //게임매니저 찾기
+        _mainCamera = Camera.main; //메인카메라 찾기
     }
 
     // Update is called once per frame
@@ -27,5 +33,9 @@ public class Timer : MonoBehaviour
         {
             TimeTxtAnim.enabled = true;
         }
+        
+        //배경색 변경
+        float t = Mathf.Clamp01((time - WarningBackground) / ColorChangeDuration * 2); //보간에 사용될 시간 값을 계산
+        _mainCamera.backgroundColor = Color.Lerp(StartColor, EndColor, t);
     }
 }
