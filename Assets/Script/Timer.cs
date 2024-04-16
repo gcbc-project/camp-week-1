bb1eac2eadd12970392b81fdf4ecf4d8d0e9a29b
YooSeungA52 <124012310+YooSeungA52@users.noreply.github.com>
2024-04-16 15:15:10 +0900

    Timer 스크립트 코드 수정
    _colorChangeDuration 변수 public → private 변경 및 코드 정리

--- Assets/Script/Timer.cs
+++ Assets/Script/Timer.cs
@@ -7,17 +7,15 @@ public class Timer : MonoBehaviour
 {
     public Text TimeTxt;
     public Animator TimeTxtAnim;
-    public float WarningTime = 20.0f; //타이머 애니메이션 시작할 시간
-    public float WarningBackground = 25.0f; //애니메이션 시작할 시간
+    public Color StartColor;
+    public Color EndColor;
 
     private GameManager _gameManager; //게임매니저에 접근하기 위한 변수
     private Camera _mainCamera; //메인카메라
+    private float _warningTime = 20.0f; //타이머 애니메이션 시작할 시간
+    private float _warningBackground = 25.0f; //애니메이션 시작할 시간
+    private float _colorChangeDuration = 10.0f; //배경색 변경에 걸리는 시간
 
-    public Color StartColor;
-    public Color EndColor;
-    public float ColorChangeDuration = 10.0f; //배경색 변경에 걸리는 시간
-
-    // Start is called before the first frame update
     void Start()
     {
         _gameManager = FindObjectOfType<GameManager>(); //게임매니저 찾기
@@ -29,13 +27,13 @@ public class Timer : MonoBehaviour
     {
         float time = _gameManager.GetTime(); //time에 시간 넣기
 
-        if(time >= WarningTime)
+        if(time >= _warningTime)
         {
             TimeTxtAnim.enabled = true;
         }
-        
-        //배경색 변경
-        float t = Mathf.Clamp01((time - WarningBackground) / ColorChangeDuration * 2); //보간에 사용될 시간 값을 계산
+
+        //배경색 변경           (time - 시작 시간) / 변경에 걸리는 시간
+        float t = Mathf.Clamp01((time - _warningBackground) / _colorChangeDuration * 2); //보간에 사용될 시간 값을 계산
         _mainCamera.backgroundColor = Color.Lerp(StartColor, EndColor, t);
     }
 }
