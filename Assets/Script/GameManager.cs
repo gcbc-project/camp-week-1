1fef93581ef4797ec662ca4ee7f34c8a54d705c1
qaws1234plok <qaws915@gmail.com>
2024-04-16 12:52:27 +0900

    feature/fail-time

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -31,23 +31,13 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (time > 30.0f)
+        if (time >= 30.0f)
         {
-            EndTxt.SetActive(true);
-            Time.timeScale = 0.0f;
-
-
-            // 시간이 30초를 초과하거나 같으면 게임오버 처리
-            if (time >= 30.0f)
-            {
-                GameOver();
-            }
+            GameOver();
         }
 
-        // 30�� ����� ���� ���� �� ���� ���� �ǳ�
-
-        time += Time.deltaTime; // �ð� �帧
-        TimeTxt.text = time.ToString("N2"); // �ð� �帥 ��ŭ �ݿ�
+        time += Time.deltaTime; // 시간초
+        TimeTxt.text = time.ToString("N2"); // 시간초를 시간판에 반영
     }
 
     public void Matched()
@@ -57,9 +47,13 @@ public class GameManager : MonoBehaviour
             FirstCard.OnDestroyCard();
             SecondCard.OnDestroyCard();
             CardCount -= 2;
+
+            // 본래 게임 오버 함수를 출력하나, 카드 카운터가 0일시 시간이 30초 보다 적어도 30.00으로 만드므로, 게임오버를 호출하는게 아닌 따로 게임 끝 판넬과 시간 멈춤을
+            // 부여
             if (CardCount == 0)
             {
-                GameOver();
+                EndTxt.SetActive(true);
+                Time.timeScale = 0.0f;
             }
         }
         else
@@ -67,14 +61,9 @@ public class GameManager : MonoBehaviour
             FirstCard.OnCloseCard();
             SecondCard.OnCloseCard();
 
-            // 30초 이하일 때만 패널티 적용
-            if (time < 30.0f)
-            {
-                float newTime = time + 5.0f;
+            // 시간 패널티
+            time += 5.0f;
 
-                // 30초를 초과하지 않도록 수정
-                time = Math.Min(newTime, 30.0f);
-            }
             FirstCard = null;
             SecondCard = null;
         }
@@ -83,10 +72,16 @@ public class GameManager : MonoBehaviour
     // 게임오버 함수를 밖으로 빼냄, 이를 통해 윗 구간에서 게임오버를 호출 할 수 있도록 바꿈
     void GameOver()
     {
-        // 시간을 정확히 30초로 설정
+        // 게임 오버 호출 시 시간은 무조건 30.00f로 바꾼다. (32초든 31초든)
         time = 30.00f;
+
+        // 바꾼 시간을 시간판에 반영한다.
         TimeTxt.text = time.ToString("N2");
+
+        // 게임 끝 판넬을 불러온다
         EndTxt.SetActive(true);
+
+        // 시간을 멈춘다
         Time.timeScale = 0.0f;
     }
 
