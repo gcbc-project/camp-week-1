04f844062ba2f66084feb4e93271ab50cb3d6108
l11040 <113540683+l11040@users.noreply.github.com>
2024-04-16 20:02:26 +0900

    보드 중앙정렬 및 동적으로 배열이 바뀌도록 수정

--- Assets/Script/Board.cs
+++ Assets/Script/Board.cs
@@ -26,9 +26,16 @@ public class CardInfo       // 카드에 이미지와 이름을 넣기위한 클
 }
 public class Board : MonoBehaviour
 {
+    // 3*6 4*6 5*6
     public GameObject Card;
-    void Start()//ī�� ���� �迭�� �ε��� ����
-    {   // 30장의 카드에 이미지 번호와 팀원 이름을 넣어준다
+    public int BoardX = 3;
+    void Start()
+    {
+        int cardAmount = BoardX * 6;
+        int numRows = 6;
+        float cardSpacing = 1.1f;
+        float cardYSpacing = 2.0f;
+
         CardInfo[] cardInfos = {new CardInfo(0, "박재민"), new CardInfo(0, "박재민"),
         new CardInfo(1, "이종윤"),new CardInfo(1, "이종윤"),
         new CardInfo(2, "박재민"),new CardInfo(2, "박재민"),
@@ -45,14 +52,15 @@ public class Board : MonoBehaviour
         new CardInfo(13, "이인호"),new CardInfo(13, "이인호"),
         new CardInfo(14, "김태형"),new CardInfo(14, "김태형")};
 
-        cardInfos = cardInfos.OrderBy(x => Random.Range(0f, 14f)).ToArray();    // 그 30장의 카드를 랜덤으로 섞어준 다음 다시 배열로 나타낸다
+        cardInfos = cardInfos.Skip(0).Take(cardAmount).ToArray();
+        cardInfos = cardInfos.OrderBy(x => Random.Range(0f, cardAmount / 2 - 1)).ToArray();
 
-        for (int i = 0; i < 30; i++)
+        for (int i = 0; i < cardAmount; i++)
         {
             GameObject tempCard = Instantiate(Card, this.transform);
 
-            float x = (i % 5) * 1.1f - 2.2f;
-            float y = (i / 5) * 1.1f - 4.0f;
+            float x = (i % BoardX - (BoardX - 1) / 2.0f) * cardSpacing;
+            float y = (numRows / 2.0f - i / BoardX) * cardSpacing - cardYSpacing;
 
             tempCard.transform.position = new Vector2(x, y);
             tempCard.GetComponent<Card>().OnCardSetting(cardInfos[i]);
