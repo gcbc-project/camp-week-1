7580601ed04c4e909f1f63879f956231460ecace
YooSeungA52 <124012310+YooSeungA52@users.noreply.github.com>
2024-04-18 20:35:26 +0900

    불필요한 코드 정리

--- Assets/Script/Hint.cs
+++ Assets/Script/Hint.cs
@@ -7,45 +7,41 @@ public class Hint : MonoBehaviour
 {
     public Animator HintAnim;
 
-    [SerializeField] private float MatchTimeout = 5f; //���� ��Ī�� ���� �ð�
+    [SerializeField] private float MatchTimeout = 5f; //성공 매칭이 없는 시간
     private float activeHintTime = 0.0f;
     private bool isHint = false;
-    private GameManager _gameManager; //���ӸŴ����� �����ϱ� ���� ����
+    private GameManager _gameManager; //게임매니저에 접근하기 위한 변수
 
     // Start is called before the first frame update
     void Start()
     {
-        _gameManager = GetComponent<GameManager>();//���ӸŴ��� ã��
+        _gameManager = GetComponent<GameManager>();//게임매니저 찾기
     }
 
     // Update is called once per frame
     void Update()
     {
         float time = _gameManager.GlobalTime;
-        //_lastMatchTime = _gameManager.MatchedTime;
 
-        // CardCount�� ��ȭ���� ���� ���
         if (time - _gameManager.MatchedTime >= MatchTimeout && time - activeHintTime >= MatchTimeout && isHint == false)
         {
-            //_iastCardCountChangeTime = Time.time;
             isHint = true;
             HintTwinkle();
 
             activeHintTime = _gameManager.GlobalTime;
             isHint = false;
         }
-
     }
 
     void HintTwinkle()
     {
         Board.CardObject = Board.CardObject.Where(card => card != null).ToList();
 
-        int randomIdx = Random.Range(0, Board.CardObject.Count); // Board.CardObject ����Ʈ�� �ε��� �� �ϳ��� �������� ����
+        int randomIdx = Random.Range(0, Board.CardObject.Count); // Board.CardObject 리스트의 인덱스 중 하나를 무작위로 선택
 
-        int tempCardIdx = Board.CardObject[randomIdx].Index; //1���� �ε����� �� ī�� �ε����� ������
+        int tempCardIdx = Board.CardObject[randomIdx].Index; //1개의 인덱스가 들어간 카드 인덱스를 가져옴
 
-        List<Card> cardList = Board.CardObject.Where(card => card.Index == tempCardIdx).ToList(); //�������� ���� ���� ���� �ε��� ī�� 2���� ������
+        List<Card> cardList = Board.CardObject.Where(card => card.Index == tempCardIdx).ToList(); //랜덤으로 뽑은 같은 값의 인덱스 카드 2개를 가져옴
 
         Color rColor = new Color(Random.value, Random.value, Random.value);
 
