0eac409c2c1728ff1e4f2c5defce92c70bb49e14
pjm0720 <woals720@naver.com>
2024-04-18 15:24:36 +0900

    시작 씬 추가 기능 구현,스테이지 선택 씬 제작
    1.시작 씬 게임 종료 버튼
    2.시작 씬 데이터 초기화 버튼
    3. 스테이지 선택 씬 제작
    -스테이지 선택 글자 추가
    -스테이지 별 선택 버튼 제작
    -스테이지 해금기능 임시 구현
    -스테이지 이름 및 최고 점수 표시 임시 구현

--- Assets/Script/RetryButton.cs
+++ Assets/Script/RetryButton.cs
@@ -14,4 +14,40 @@ public class RetryBtn : MonoBehaviour
     {
         SceneManager.LoadScene("StageSelectScene");
     }
+
+    public void OnEnterStage1()             //스테이지 1 씬으로 넘어가기
+    {
+        SceneManager.LoadScene("StageLevel1");
+    }
+
+    public void OnEnterStage2()             //스테이지 2 씬으로 넘어가기
+    {
+        SceneManager.LoadScene("StageLevel2");
+    }
+
+    public void OnEnterStage3()             //스테이지 3 씬으로 넘어가기
+    {
+        SceneManager.LoadScene("StageLevel3");
+    }
+
+    public void GameExit()      // 게임 종료 기능
+    {
+        Application.Quit(); // 게임 종료 기능 
+        //유니티 에디터도 종료되게 해주는 기능
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;    //에디터 상으로 게임이 종료되게 해준다.
+        #else
+            Application.Quit();
+        #endif
+    }
+
+    public void DeleteData()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+
+    public void OnEnterStartScene()
+    {
+        SceneManager.LoadScene("StartScene");
+    }
 }
